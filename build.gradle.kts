714911d89d75f7e0b4cf03dd4200cade3e3ff173
Bombele Liyama Camille <94522767+Bombele@users.noreply.github.com>
2025-12-29 19:37:31 -0400

    Actualizar build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,25 @@
-// build.gradle.kts
 plugins {
-    kotlin("jvm") version "1.9.24"
+    id("org.jetbrains.kotlin.jvm") version "1.8.21" // Version stable de repli
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "com.fardc.sigint"
 version = "1.0"
 
-repositories { mavenCentral() }
+repositories {
+    mavenCentral()
+}
 
-dependencies { implementation(kotlin("stdlib")) }
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("io.javalin:javalin:5.6.1")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+}
 
-kotlin { jvmToolchain(17) }
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xno-param-assertions", "-Xno-call-assertions")
-    }
+// Verrouillage de la toolchain contre l'empoisonnement Java 25
+kotlin {
+    jvmToolchain(17)
 }
 
 tasks.shadowJar {
