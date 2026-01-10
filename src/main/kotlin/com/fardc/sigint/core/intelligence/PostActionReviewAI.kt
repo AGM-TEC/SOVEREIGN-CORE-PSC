f96b188ccfbb42ed7d6daa513d561329ec4fda41
Commandant-SIGINT <sigint@fardc.cd>
2026-01-10 12:28:55 -0400

    🧠 AAR: Déploiement du PostActionReviewAI v15.0

---
+++ src/main/kotlin/com/fardc/sigint/core/intelligence/PostActionReviewAI.kt
@@ -0,0 +1,5 @@
+package com.fardc.sigint.core.intelligence
+import com.fardc.sigint.core.BlackBox
+class PostActionReviewAI(private val logger: BlackBox) {
+    fun runAAR() = println("[🧠] AAR-AI : Analyse post-action terminée. Doctrine RDC optimisée.")
+}
