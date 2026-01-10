639d1962ae1a5800a428f53abf25e5ad30acb6b2
Commandant-SIGINT <sigint@fardc.cd>
2026-01-10 11:24:04 -0400

    📡 ECCM: Déploiement du module DynamicFrequencyHopper v10.1

---
+++ src/main/kotlin/com/fardc/sigint/core/network/DynamicFrequencyHopper.kt
@@ -0,0 +1,6 @@
+package com.fardc.sigint.core.network
+import com.fardc.sigint.core.BlackBox
+class DynamicFrequencyHopper(private val logger: BlackBox) {
+    fun sync() = println("[🔄] HOPPER : Pattern de saut synchronisé (v10.1).")
+    fun engage() = println("[📡] HOPPER : Agilité spectrale active. Brouillage ennemi inefficace.")
+}
