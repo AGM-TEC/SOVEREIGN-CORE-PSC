4e5b0a3aceffb3c33bdc4685fb524dc6353fa110
Commandant-SIGINT <sigint@fardc.cd>
2026-01-10 12:58:00 -0400

    ⚡ ECM: Déploiement du module ElectronicCounterMeasures v18.0 [Offensive EW]

---
+++ src/main/kotlin/com/fardc/sigint/core/network/ElectronicCounterMeasures.kt
@@ -0,0 +1,8 @@
+package com.fardc.sigint.core.network
+import com.fardc.sigint.core.BlackBox
+class ElectronicCounterMeasures(private val logger: BlackBox) {
+    fun engage() {
+        println("[📡] ECM-ACTIVE : Attaque électronique et neutralisation drone opérationnelles.")
+        println("[⚡] STATUS : Capacité de déni de spectre v18.0 active.")
+    }
+}
