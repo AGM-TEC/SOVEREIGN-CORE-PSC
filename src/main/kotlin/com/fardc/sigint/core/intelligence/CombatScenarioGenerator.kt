afa70aaede44dc723344e431e799d9830f736a39
Commandant-SIGINT <sigint@fardc.cd>
2026-01-10 12:11:15 -0400

    🎲 DOCTRINE: Déploiement du CombatScenarioGenerator v14.0

---
+++ src/main/kotlin/com/fardc/sigint/core/intelligence/CombatScenarioGenerator.kt
@@ -0,0 +1,5 @@
+package com.fardc.sigint.core.intelligence
+import com.fardc.sigint.core.BlackBox
+class CombatScenarioGenerator(private val logger: BlackBox) {
+    fun generate() = println("[🎲] GENERATOR : Scénario d'attaque hybride généré (Qualité Industrielle v14.0).")
+}
