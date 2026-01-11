f4b96ecae4d9dad521a559786e304df22ea35606
Commandant-SIGINT <sigint@fardc.cd>
2026-01-11 17:08:18 -0400

    🗺️ MAP: Déploiement OfflineMapProvider v26.2 [Military Standard]

--- ui/map/OfflineMapProvider.kt
+++ ui/map/OfflineMapProvider.kt
@@ -1,1 +1,8 @@
-
+package com.fardc.sigint.ui.map
+import com.fardc.sigint.core.BlackBox
+class OfflineMapProvider(private val logger: BlackBox) {
+    fun engage() {
+        println("[🗺️] OFFLINE-MAP-PROVIDER v26.2 : Cartographie souveraine déconnectée active.")
+        println("[🇨🇩] INDÉPENDANCE : Navigation garantie sans accès Internet.")
+    }
+}
