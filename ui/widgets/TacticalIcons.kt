600fe92e16d0af9b7124deddaec06638431d2ee9
Commandant-SIGINT <sigint@fardc.cd>
2026-01-11 17:37:24 -0400

    🖥️ UI: Déploiement TacticalIcons v27.3 [Military Symbology]

--- ui/widgets/TacticalIcons.kt
+++ ui/widgets/TacticalIcons.kt
@@ -1,1 +1,7 @@
-
+package com.fardc.sigint.ui.widgets
+class TacticalIcons {
+    fun engage() {
+        println("[🎨] TACTICAL-ICONS v27.3 : Moteur de symbologie MIL-STD-2525D actif.")
+        println("[🇨🇩] VISUALISATION : Standardisation du langage de combat FARDC réussie.")
+    }
+}
