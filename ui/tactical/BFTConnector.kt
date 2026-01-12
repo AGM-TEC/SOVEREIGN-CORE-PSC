567ac3c32eb443813c4daa0852123ff9c804a593
Commandant-SIGINT <sigint@fardc.cd>
2026-01-12 10:10:41 -0400

    🖥️ UI: Déploiement BFTConnector v29.2 [Blue Force Tracking]

---
+++ ui/tactical/BFTConnector.kt
@@ -0,0 +1,9 @@
+package com.fardc.sigint.ui.tactical
+import com.fardc.sigint.core.network.NetworkEngine
+import com.fardc.sigint.core.BlackBox
+class BFTConnector(private val network: NetworkEngine, private val logger: BlackBox) {
+    fun engage() {
+        println("[📍] BFT-CONNECTOR v29.2 : Radar tactique allié opérationnel.")
+        println("[🇨🇩] UNITÉ : Coordination inter-armes activée.")
+    }
+}
