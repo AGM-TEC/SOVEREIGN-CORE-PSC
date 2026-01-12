e3c44d0bbfaddececa9eb27f314491738c9da628
Commandant-SIGINT <sigint@fardc.cd>
2026-01-12 10:30:35 -0400

    🖥️ UI: Déploiement NightVisionTheme v29.3 [Stealth Vision]

--- ui/tactical/NightVisionTheme.kt
+++ ui/tactical/NightVisionTheme.kt
@@ -1,1 +1,9 @@
-
+package com.fardc.sigint.ui.tactical
+import com.fardc.sigint.core.hardware.SovereignHAL
+import com.fardc.sigint.core.BlackBox
+class NightVisionTheme(private val hal: SovereignHAL, private val logger: BlackBox) {
+    fun engage() {
+        println("[🌑] NIGHT-VISION-THEME v29.3 : Discrétion lumineuse et mode JVN actifs.")
+        println("[🇨🇩] SURVIE : Signature visuelle du terminal réduite au minimum.")
+    }
+}
